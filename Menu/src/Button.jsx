f1e8b57cd32fd572f9f749b8432cbf65e881302e
Sasa Nedic <svetasmak0@gmail.com>
2024-03-02 23:58:35 +0100

    Finish project

--- Menu/src/Button.jsx
+++ Menu/src/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button ({children}) {
     return(
-    <button>{children}</button>
+    <button className="btn">{children}</button>
     )
 }
--- Menu/src/Item.jsx
+++ Menu/src/Item.jsx
@@ -1,15 +1,14 @@
 
 export default function Item({ data }) {
     return (
-        <div>
+        <div className="item">
             {data.map((item) => (
                 <li key={item.id}>
-                    <div>
-                        <p>{item.id}</p>
-                        <p>Title: {item.title}</p>
-                        <p>Category: {item.category}</p>
-                        <p>Price: {item.price}</p>
-                        <p>Description: {item.desc}</p>
+                    <div className="item-information">
+                        <p><span>Title:</span> {item.title}</p>
+                        <p><span>Category:</span> {item.category}</p>
+                        <p><span>Price:</span> {item.price}</p>
+                        <p><span>Description:</span> {item.desc}</p>
                     </div>
                 </li>
             ))}
