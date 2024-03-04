53e7b9635b030249afd00bb9da1eb7dae0646a33
Sasa Nedic <svetasmak0@gmail.com>
2024-03-04 17:33:40 +0100

    Create generateRandomColor function

--- Menu/src/Item.jsx
+++ Menu/src/Item.jsx
@@ -1,6 +1,12 @@
 import Button from "./Button";
 import SingleItem from "./SingleItem";
-export default function Item({ data,  }) {
+export default function Item({ data, setBorderColor,borderColor }) {
+    function generateRandomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
 
         return (
             <>
