7b90e41e8f3fcbb73c11a887141eaa15646ce233
Sasa Nedic <svetasmak0@gmail.com>
2024-03-04 14:13:49 +0100

    Change code logic

--- Menu/src/Item.jsx
+++ Menu/src/Item.jsx
@@ -1,16 +1,9 @@
-
+import SingleItem from "./SingleItem";
 export default function Item({ data }) {
     return (
         <div className="item">
             {data.map((item) => (
-                <li key={item.id}>
-                    <div className="item-information">
-                        <p className="title"> {item.title}</p>
-                        <p><span>Category:</span> {item.category}</p>
-                        <p><span>Price:</span> {item.price}</p>
-                        <p><span>Description:</span> {item.desc}</p>
-                    </div>
-                </li>
+           <SingleItem {...item}/>
             ))}
         </div>
     );
---
+++ Menu/src/SingleItem.jsx
@@ -0,0 +1,14 @@
+const SingleItem = (item) => {
+    console.log(item)
+    return(
+        <li key={item.id}>
+        <div className="item-information">
+            <p className="title"> {item.title}</p>
+            <p><span>Category:</span> {item.category}</p>
+            <p><span>Price:</span> {item.price}</p>
+            <p><span>Description:</span> {item.desc}</p>
+        </div>
+    </li>
+    )
+}
+export default SingleItem;
